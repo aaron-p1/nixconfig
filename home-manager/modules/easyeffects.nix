0af145c25a8c7c82d4678c9c93b9b40075c70a2d
Aaron Pietscher <aaron.pietscher@gmail.com>
2021-10-06 15:45:37 +0200

    [home-manager, easyeffects] fix rnnoise paths

--- home-manager/modules/easyeffects.nix
+++ home-manager/modules/easyeffects.nix
@@ -1,12 +1,34 @@
-{ pkgs, ... }:
-{
+{ pkgs, lib, config, ... }:
+let
+  # get file names of directory
+  getFiles = (lib.flip lib.pipe) (with builtins; [ readDir attrNames ]);
+
+  # get all input and output files
+  profileNames = builtins.foldl' (acc: dir: 
+  acc ++ builtins.map (file: "easyeffects/${dir}/" + file) (getFiles (../../dotfiles/easyeffects + "/${dir}"))
+  ) [] [ "input" "output" ];
+
+  # replace text
+  profiles = builtins.foldl' (acc: file:
+  let
+    fileText = builtins.readFile (../../dotfiles + "/${file}");
+    replacedFileText = builtins.replaceStrings [ "{config}" ] [ (builtins.toString config.xdg.configHome) ] fileText;
+  in
+  acc // {
+    "${file}".text = replacedFileText;
+  }
+  ) {} profileNames;
+
+in {
   services.easyeffects = {
     enable = true;
     preset = "Nothing";
   };
 
-  xdg.configFile."easyeffects" = {
-    source = ../../dotfiles/easyeffects;
-    recursive = true;
+  xdg.configFile = lib.recursiveUpdate profiles {
+    "easyeffects/rnnoise" = {
+      source = ../../dotfiles/easyeffects/rnnoise;
+      recursive = true;
+    };
   };
 }
