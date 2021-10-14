54e24158dd2c19aa7ccc8837b2971495d9fbd714
Aaron Pietscher <aaron.pietscher@gmail.com>
2021-10-14 17:50:10 +0200

    [nixos, host, aaron-pc] change cpuFreqGovernor to ondemand

--- hosts/aaron-pc/configuration.nix
+++ hosts/aaron-pc/configuration.nix
@@ -5,6 +5,8 @@
       ./hardware-configuration.nix
     ];
 
+  powerManagement.cpuFreqGovernor = "ondemand";
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
