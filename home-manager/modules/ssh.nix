b9656b160b7cd48fef73b6d1be01ca8cbf31e724
Aaron Pietscher <aaron.pietscher@gmail.com>
2021-10-19 08:46:13 +0200

    [home-manager, ssh] add host configs for home server

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -2,5 +2,17 @@
 {
   programs.ssh = {
     enable = true;
+    matchBlocks = {
+      public-server = {
+        hostname = "public-server";
+        user = "aaron";
+        port = 25566;
+      };
+      public-server-root = {
+        hostname = "public-server";
+        user = "root";
+        port = 25566;
+      };
+    };
   };
 }
