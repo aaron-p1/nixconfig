cd61cec4acdc1dbbedaeeae3524ace9cb4824d19
Aaron Pietscher <aaron.pietscher@gmail.com>
2021-11-02 22:49:13 +0100

    [dotfiles, nvim, autopairs] fix cmp integration

--- dotfiles/nvim/lua/plugins/autopairs.lua
+++ dotfiles/nvim/lua/plugins/autopairs.lua
@@ -4,11 +4,16 @@ function plugin.config()
 	require('nvim-autopairs').setup{
 		disable_filetype = {'TelescopePrompt', 'dap-repl', 'dapui_watches'}
 	}
-	require("nvim-autopairs.completion.cmp").setup({
-		map_cr = true, --  map <CR> on insert mode
-		map_complete = true, -- it will auto insert `(` after select function or method item
-		auto_select = true -- automatically select the first item
-	})
+
+	local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+	local cmp = require('cmp')
+	cmp.event:on(
+		'confirm_done',
+		cmp_autopairs.on_confirm_done({
+				map_cr = true, --  map <CR> on insert mode
+				map_complete = true, -- it will auto insert `(` after select function or method item
+				auto_select = true -- automatically select the first item
+		}))
 end
 
 return plugin
