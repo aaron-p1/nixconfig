4e96a999ad7179d36901bcbc88f65c600c5cf7ce
Aaron Pietscher <aaron.pietscher@gmail.com>
2021-11-09 11:02:23 +0100

    [nixos, podman] add podman-compose

--- nixos/modules/podman.nix
+++ nixos/modules/podman.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   virtualisation.podman = {
     enable = true;
     enableNvidia = true;
     dockerCompat = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    podman-compose
+  ];
 }
