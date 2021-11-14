655ad571d59240c047a5c7f1761f2c6a712f2642
Aaron Pietscher <aaron.pietscher@gmail.com>
2021-11-14 22:19:38 +0100

    [home-manager, mpv] add ytdl options

--- home-manager/modules/mpv.nix
+++ home-manager/modules/mpv.nix
@@ -1,4 +1,28 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  inherit (lib) mapAttrs' nameValuePair foldr mapAttrsToList flip pipe;
+  inherit (builtins) concatStringsSep;
+
+  # String -> AttrSet -> AttrSet
+  addKeyPrefix = (pre: mapAttrs' (k: v: nameValuePair (pre + "-" + k) (v)));
+  # [AttrSet] -> AttrSet
+  concatAttrs = foldr (a: acc: acc // a) {};
+  # AttrSet -> [String]
+  toStringList = mapAttrsToList (k: v: k + "=" + (toString v));
+
+  # AttrSet -> String
+  toListOptions = flip pipe [
+    toStringList # AttrSet to [ "key=value" ... ]
+    (concatStringsSep ",")
+  ];
+
+  # AttrSet -> String
+  toListOptionsPrefix = flip pipe [
+    (mapAttrsToList addKeyPrefix) # { prefix.abc = 1 } -> [ { prefix-abc = 1 } ]
+    concatAttrs
+    toListOptions
+  ];
+in
 {
   programs.mpv = {
     enable = true;
@@ -12,7 +36,16 @@
     config = {
       volume = 100;
       osc = "no"; # mpv_thumbnail_script_client_osc
-      ytdl_path = "${pkgs.yt-dlp}/bin/yt-dlp";
+      script-opts = toListOptionsPrefix {
+        ytdl_hook = {
+          ytdl_path = "${pkgs.yt-dlp}/bin/yt-dlp";
+        };
+      };
+      ytdl-raw-options = toListOptions {
+        sub-lang = "\"en,de\"";
+        write-sub = "";
+        write-auto-sub = "";
+      };
     };
 
     profiles = {
