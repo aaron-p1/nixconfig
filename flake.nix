94904cc262ae6cea5cebd081b54d6fe92956924d
Aaron Pietscher <aaron.pietscher@gmail.com>
2021-12-21 20:40:20 +0100

    format flake.nix and localpkgs

--- flake.nix
+++ flake.nix
@@ -4,16 +4,9 @@
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.follows = "unstable";
 
-    localpkgs = {
-      url = "path:./localpkgs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nur.url = "github:nix-community/NUR";
-
-    nixos-hardware.url = "github:nixos/nixos-hardware";
-
     flake-utils.url = "github:numtide/flake-utils";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+    nur.url = "github:nix-community/NUR";
 
     home-manager = {
       url = "github:rycee/home-manager";
@@ -24,64 +17,63 @@
       url = "github:nix-community/neovim-nightly-overlay";
       inputs.nixpkgs.follows = "unstable";
     };
+
+    # LOCAL PACKAGES
+    jdt-ls.url =
+      "github:nixos/nixpkgs?rev=3ae6abea23628f16a1c60ce9f9ec36deb0b5a59e";
   };
 
-  outputs = { self, nixpkgs, stable, nur, localpkgs, nixos-hardware, flake-utils, ...}@inputs:
-  let
-    inherit (inputs.unstable) lib; # unstable for home manager
-    overlays = [
-      inputs.neovim-nightly-overlay.overlay
-      nur.overlay
-      localpkgs.overlay
-      (final: prev: {
-        stable = import stable { inherit (final) system; };
-      })
-    ];
-  in {
-    nixosConfigurations = {
-      aaron-pc = lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          { nixpkgs.overlays = overlays; }
-
-          nixos-hardware.nixosModules.common-pc-ssd
-          nixos-hardware.nixosModules.common-cpu-intel
-
-          ./nixos/configs/main.nix
-          ./hosts/aaron-pc/configuration.nix
-
-          inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.aaron = {
-                imports = [
-                  ./home-manager/configs/main.nix
-                  ./hosts/aaron-pc/home.nix
-                ];
-              };
-            };
-          }
-        ];
-        extraArgs = { inherit inputs; };
-      };
-    };
-  }
-  //
-  flake-utils.lib.eachDefaultSystem (system: 
-  let
-    pkgs = nixpkgs.legacyPackages."${system}";
-  in {
-    devShell = pkgs.mkShell {
-      packages = with pkgs; [
-        gnumake
-        rsync
-
-        # lsp
-        sumneko-lua-language-server
+  outputs = { self, stable, unstable, nixpkgs, flake-utils, nixos-hardware, nur
+    , home-manager, neovim-nightly-overlay, ... }@inputs:
+    let
+      inherit (unstable) lib; # unstable for home manager
+      overlays = [
+        (final: prev: { stable = import stable { inherit (final) system; }; })
+        nur.overlay
+        neovim-nightly-overlay.overlay
+        (import ./localpkgs { inherit inputs; })
       ];
-    };
-  }
-  );
+    in {
+      nixosConfigurations = {
+        aaron-pc = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            { _module.args = { inherit inputs; }; }
+            { nixpkgs.overlays = overlays; }
+
+            nixos-hardware.nixosModules.common-pc-ssd
+            nixos-hardware.nixosModules.common-cpu-intel
+
+            ./nixos/configs/main.nix
+            ./hosts/aaron-pc/configuration.nix
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.aaron = {
+                  imports = [
+                    ./home-manager/configs/main.nix
+                    ./hosts/aaron-pc/home.nix
+                  ];
+                };
+              };
+            }
+          ];
+        };
+      };
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages."${system}";
+      in {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            gnumake
+            rsync
+
+            # lsp
+            sumneko-lua-language-server
+          ];
+        };
+      });
 }
---
+++ localpkgs/default.nix
@@ -0,0 +1,23 @@
+{ inputs, ... }:
+let
+  inherit (inputs) jdt-ls;
+in
+final: prev: {
+  local = rec {
+    gotmux = prev.callPackage ./pkgs/gotmux { };
+
+    # firefox native messaging hosts
+    ff2mpv-native-client = prev.callPackage ./pkgs/ff2mpv-native-client { };
+
+    # java language server
+    inherit (jdt-ls.legacyPackages."${final.system}") jdt-language-server;
+
+    # Nvidia
+    nvlax = prev.callPackage ./pkgs/nvlax { };
+    # patched with https://github.com/keylase/nvidia-patch
+    nvidia_x11 = linuxPackages:
+      prev.callPackage ./overrides/nvidia_x11-fbc {
+        inherit linuxPackages nvlax;
+      };
+  };
+}
