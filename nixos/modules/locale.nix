936785038009ec1eb6e52597209139116c7855c6
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-01-18 21:43:09 +0100

    [nixos, locale] add german format

--- nixos/modules/locale.nix
+++ nixos/modules/locale.nix
@@ -10,7 +10,24 @@ with lib; {
   config = mkIf cfg.enable {
     time.timeZone = "Europe/Berlin";
 
-    i18n.defaultLocale = "en_US.UTF-8";
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
+      extraLocaleSettings = {
+        LC_CTYPE = "de_DE.UTF-8";
+        LC_NUMERIC = "de_DE.UTF-8";
+        LC_TIME = "de_DE.UTF-8";
+        LC_COLLATE = "de_DE.UTF-8";
+        LC_MONETARY = "de_DE.UTF-8";
+        LC_MESSAGES = "de_DE.UTF-8";
+        LC_PAPER = "de_DE.UTF-8";
+        LC_NAME = "de_DE.UTF-8";
+        LC_ADDRESS = "de_DE.UTF-8";
+        LC_TELEPHONE = "de_DE.UTF-8";
+        LC_MEASUREMENT = "de_DE.UTF-8";
+        LC_IDENTIFICATION = "de_DE.UTF-8";
+      };
+    };
+
     console = {
       font = "Lat2-Terminus16";
       keyMap = "de";
