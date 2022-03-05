c21081cd6643f959ef2dce6267847ebe2cc1894b
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-03-05 15:58:44 +0100

    [localpkgs, initdev] add python env

---
+++ localpkgs/pkgs/initdev/envfiles/python/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A very basic flake";
+
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, flake-compat }: {
+
+    devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux;
+      mkShell {
+        packages = [
+          (python3.withPackages (p: with p; [
+          ]))
+          pyright
+        ];
+      };
+  };
+}
