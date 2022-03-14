40b9823eadd470cd39c72ad614715a5d4d46bc30
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-03-14 19:07:54 +0100

    [dotfiles, nvim, luasnip, c_like] add true false

--- dotfiles/nvim/lua/plugins/my-luasnip/c_like.lua
+++ dotfiles/nvim/lua/plugins/my-luasnip/c_like.lua
@@ -2,9 +2,12 @@ local helper = require("plugins.my-luasnip.helper")
 
 local s = helper.s
 local i = helper.i
+local t = helper.t
 local fmta = helper.fmta
 
 local snips = {
+  s("t", t("true")),
+  s("f", t("false")),
   s("if", fmta("if (<>) {\n\t<>\n}", { i(1, "true"), i(0) })),
   s("ei", fmta("else if (<>) {\n\t<>\n}", { i(1, "true"), i(0) })),
   s("el", fmta("else {\n\t<>\n}", i(0))),
