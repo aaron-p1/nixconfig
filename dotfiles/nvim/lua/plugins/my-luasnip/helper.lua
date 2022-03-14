24cc779bcdbe72bc247cafc85c73feb32b420244
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-03-14 21:46:03 +0100

    [dotfiles, nvim, luasnip, php] add init template

--- dotfiles/nvim/lua/plugins/my-luasnip/helper.lua
+++ dotfiles/nvim/lua/plugins/my-luasnip/helper.lua
@@ -1,25 +1,64 @@
 local ls = require("luasnip")
 
+-- LuaSnip
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
+
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+
+local conds = require("luasnip.extras.expand_conditions")
+
+-- Helper
+-- conditions
+local function firstLine()
+  return vim.fn.line(".") == 1
+end
+
+local function firstInFile(line_to_cursor, matched_trigger)
+  return firstLine() and line_to_cursor == matched_trigger
+end
+
 return {
   ls = ls,
-  s = ls.snippet,
-  sn = ls.snippet_node,
-  isn = ls.indent_snippet_node,
-  t = ls.text_node,
-  i = ls.insert_node,
-  f = ls.function_node,
-  c = ls.choice_node,
-  d = ls.dynamic_node,
-  r = ls.restore_node,
-  events = require("luasnip.util.events"),
-  ai = require("luasnip.nodes.absolute_indexer"),
+  s = s,
+  sn = sn,
+  isn = isn,
+  t = t,
+  i = i,
+  f = f,
+  c = c,
+  d = d,
+  r = r,
+  events = events,
+  ai = ai,
 
-  l = require("luasnip.extras").lambda,
-  rep = require("luasnip.extras").rep,
-  p = require("luasnip.extras").partial,
-  m = require("luasnip.extras").match,
-  n = require("luasnip.extras").nonempty,
-  dl = require("luasnip.extras").dynamic_lambda,
-  fmt = require("luasnip.extras.fmt").fmt,
-  fmta = require("luasnip.extras.fmt").fmta,
+  l = l,
+  rep = rep,
+  p = p,
+  m = m,
+  n = n,
+  dl = dl,
+  fmt = fmt,
+  fmta = fmta,
+
+  conds = conds,
+
+  firstLine = firstLine,
+  firstInFile = firstInFile,
 }
--- dotfiles/nvim/lua/plugins/my-luasnip/php.lua
+++ dotfiles/nvim/lua/plugins/my-luasnip/php.lua
@@ -1,14 +1,97 @@
-local helper = require("plugins.my-luasnip.helper")
+local fun = require("fun")
+local helper = require("helper")
 
-local s = helper.s
-local t = helper.t
-local i = helper.i
-local c = helper.c
+local lsh = require("plugins.my-luasnip.helper")
 
-local fmta = helper.fmta
+local s = lsh.s
+local sn = lsh.sn
+local t = lsh.t
+local i = lsh.i
+local c = lsh.c
+local d = lsh.d
+
+local fmta = lsh.fmta
+
+local firstLine = lsh.firstLine
+local firstInFile = lsh.firstInFile
+
+local function getNamespaceFromPath()
+  local path = vim.fn.expand("%:h")
+
+  if path == "." then
+    return nil
+  end
+
+  return fun.iter(vim.fn.split(path, "/"))
+    :map(function(dir)
+      return dir:gsub("^%l", string.upper)
+    end)
+    :foldl(function(acc, v)
+      if acc == nil then
+        return v
+      end
+      return acc .. "\\" .. v
+    end, nil)
+end
+
+local function getNamespaceFromFile()
+  local path = vim.fn.expand("%:h")
+  local filename = vim.fn.expand("%:t")
+
+  local file_to_read = fun.iter(vim.fn.readdir(path))
+    :filter(function(file)
+      return helper.endsWith(file, ".php") and file ~= filename
+    end)
+    :nth(1)
+
+  if file_to_read == nil then
+    return nil
+  end
+
+  for _, line in pairs(vim.fn.readfile(path .. "/" .. file_to_read, "", 5)) do
+    if helper.startsWith(line, "namespace") then
+      return line:match("%w+ (.+);")
+    end
+  end
+
+  return nil
+end
+
+local function getNamespaceLine(namespace)
+  return sn(nil, { t({ "", "namespace " }), i(1, namespace), t({ ";", "" }) })
+end
+
+local function getNamespace()
+  local result = getNamespaceFromFile()
+
+  if result ~= nil then
+    return getNamespaceLine(result)
+  end
+
+  result = getNamespaceFromPath()
+
+  if result == nil then
+    return ""
+  end
+  return getNamespaceLine(result)
+end
+
+local function getClassName()
+  return sn(nil, i(1, vim.fn.expand("%:t:r")))
+end
 
 local snips = {
-  s("t", t("$this->")),
+  s(
+    "init",
+    fmta("<<?php\n<>\nclass <><>\n{\n\t<>\n}", {
+      d(1, getNamespace, {}),
+      d(2, getClassName, {}),
+      i(3),
+      i(0),
+    }),
+    { condition = firstInFile, show_condition = firstLine }
+  ),
+  s("th", t("$this->")),
   s("ufn", t("public function ")),
   s("ofn", t("protected function ")),
   s("ifn", t("private function ")),
