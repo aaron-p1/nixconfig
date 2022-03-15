d2f2bda995de0897a67dff68c684cb6b15e09e9d
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-03-15 08:35:45 +0100

    [host, aaron-laptop] add chromium

--- hosts/aaron-laptop/configuration.nix
+++ hosts/aaron-laptop/configuration.nix
@@ -29,6 +29,9 @@
     "11iNbdbRK5qw71GUhRYJ+/EQW1T/GU/X3NMjpcMHuXqkybKZ5Qa74dmlR8iXIb/6+SXwTno1oVAAJHm5IJDtvA==,AYID8itmW0xAcd+9ZCl4pHVuTQwB7Npk4XhmqVP5KFMuDfaDOG4aYi1E+p62wPdpxc6xPTPx6ZwU23jGPtutPA==,es256,+presence"
   ];
 
+
+  programs.chromium.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
