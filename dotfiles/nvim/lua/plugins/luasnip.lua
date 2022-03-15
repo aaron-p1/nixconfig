cf0b0c22f501a252fec5bbc209034cb668321bdf
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-03-15 16:51:42 +0100

    [dotfiles, nvim, luasnip] add vue

--- dotfiles/nvim/lua/plugins/luasnip.lua
+++ dotfiles/nvim/lua/plugins/luasnip.lua
@@ -96,6 +96,7 @@ function plugin.config()
     blade = { "blade", "html" },
     json = { "json" },
     javascript = { "javascript", "c_like" },
+    vue = { "javascript", "c_like" },
   }
 
   ls.snippets = fun.iter(group_assignments)
