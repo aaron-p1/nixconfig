6bf79172fd93f55497c0073cd23b1e103f287025
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-05-13 17:05:16 +0200

    [localpkgs, initdev] use native flake with filtered source

--- localpkgs/pkgs/initdev/envfiles/yarn/flake.nix
+++ localpkgs/pkgs/initdev/envfiles/yarn/flake.nix
@@ -1,15 +1,18 @@
 {
   description = "A very basic flake";
 
-  inputs.flake-compat = {
-    url = "github:edolstra/flake-compat";
-    flake = false;
-  };
-
-  outputs = { self, nixpkgs, flake-compat }: {
+  outputs = { self, nixpkgs }: {
 
     devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux;
       mkShell {
+        src = with builtins;
+          filterSource (path: type:
+            elem path (map (e: toString (./. + "/${e}")) [
+              "flake.nix"
+              "flake.lock"
+              ".envrc"
+            ])) ./.;
+
         packages = [
           nodejs
           yarn
