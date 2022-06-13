99a4308d817368751f0fada11f51a1d1c1983287
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-06-13 20:24:57 +0200

    [home-manager, ssh] export additional hosts to an encrypted file

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -1,14 +1,25 @@
 { config, lib, ... }:
-let cfg = config.within.ssh;
+let
+  cfg = config.within.ssh;
+
+  additionalHostsContent =
+    builtins.readFile ../../secrets/inline-secrets/additional-ssh-hosts.json;
+  additionalHosts = builtins.fromJSON additionalHostsContent;
+
 in with lib; {
   options.within.ssh = { enable = mkEnableOption "SSH"; };
 
   config = mkIf cfg.enable {
+    assertions = [{
+      assertion = hasPrefix "{" additionalHostsContent;
+      message = "Host file does not start with {"; # }}
+    }];
+
     programs.ssh = {
       enable = true;
       controlMaster = "auto";
       controlPath = "~/.ssh/sockets/%r@%n:%p";
-      matchBlocks = {
+      matchBlocks = recursiveUpdate {
         laptop = {
           hostname = "aaron-laptop";
           user = "aaron";
@@ -24,7 +35,7 @@ in with lib; {
           user = "root";
           port = 25566;
         };
-      };
+      } additionalHosts;
     };
   };
 }
