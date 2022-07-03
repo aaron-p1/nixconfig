8b601984fe4a1f16e8c2f542dab9f9df535aada4
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-07-03 09:33:27 +0200

    [home-manager, alacritty] set hints.alphabet to a-z

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -14,6 +14,7 @@ in with lib; {
         };
         font.size = 9;
         mouse.hide_when_typing = true;
+        hints.alphabet = "abcdefghjklmnopqrstuvwxyz";
         key_bindings = [{
           key = "F11";
           action = "ToggleFullscreen";
