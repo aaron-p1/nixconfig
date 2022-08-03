5b2e162edc81472afa24c94b4b7df638a9a8c941
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-08-03 18:34:40 +0200

    [hosts, aaron-laptop] add i915 to initrd modules

--- hosts/aaron-laptop/hardware-configuration.nix
+++ hosts/aaron-laptop/hardware-configuration.nix
@@ -4,7 +4,7 @@
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot.initrd.availableKernelModules =
-    [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" ];
+    [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "i915" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
