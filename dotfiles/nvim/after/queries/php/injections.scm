d728f3422e421b63ae37d95c2483d290ed5d84d3
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-08-23 16:40:34 +0200

    [dotfiles, nvim, queries, php, injections] add regex

--- dotfiles/nvim/after/queries/php/injections.scm
+++ dotfiles/nvim/after/queries/php/injections.scm
@@ -1,5 +1,8 @@
 ((string_value) @sql
   (#match? @sql "^(SELECT|INSERT|UPDATE|DROP)"))
 
+((string_value) @regex
+  (#lua-match? @regex "^%^"))
+
 ((comment) @comment
   (#match? @comment "^/(/|\\*[^*])"))
