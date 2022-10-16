399fa983df63ab90a944c486606d8e346c1a9fe4
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-10-16 12:09:07 +0200

    [home-manager, tmux] add key bind to kill session

--- home-manager/modules/tmux.nix
+++ home-manager/modules/tmux.nix
@@ -24,6 +24,8 @@ in with lib; {
         set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
         # Underline Color
         set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
+
+        bind-key -N "Kills session" K confirm-before -p "Kill session?" kill-session
       '';
 
       plugins = with pkgs.tmuxPlugins; [{
