66e055d04f7eda9eceb3dedd29669c3382bf7265
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-10-16 12:11:40 +0200

    [dotfiles, nvim, luasnip, file-templates, nix] fix cmake flake template

--- dotfiles/nvim/lua/plugins/luasnip/snippets/file-templates/files/nix-flake-cmake.nix
+++ dotfiles/nvim/lua/plugins/luasnip/snippets/file-templates/files/nix-flake-cmake.nix
@@ -2,20 +2,17 @@
 	description = "<>";
 
 	outputs = { self, nixpkgs }:
-	with nixpkgs.legacyPackages.x86_64-linux; {
+		with nixpkgs.legacyPackages.x86_64-linux; {
 
-		packages.x86_64-linux.default = stdenv.mkDerivation {
-			name = "<>";
-			version = "<>";
-			src = ./.;
-			buildInputs = [ cmake ];
-		};
+			packages.x86_64-linux.default = stdenv.mkDerivation {
+				name = "<>";
+				version = "<>";
+				src = ./.;
+				buildInputs = [ cmake ];
+			};
 
-		devShell.x86_64-linux = mkShell {
-			buildInputs = [ cmake cmake-language-server clang ];
-				# used in nvim
-				NVIM_CLANGD_INCLUDE = "${glibc.dev}/include"
-					+ ":${llvmPackages.clang-unwrapped.lib}/lib/clang/${clang.version}/include/";
+			devShell.x86_64-linux = mkShell {
+				buildInputs = [ cmake cmake-language-server clang-tools ];
+			};
 		};
-	};
 }
