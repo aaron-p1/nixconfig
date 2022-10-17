d66ce49d5f8d2260b2bde294035dd7f5dca7e1fe
Aaron Pietscher <aaron.pietscher@gmail.com>
2022-10-17 22:56:29 +0200

    [dotfiles, nvim, queries, fennel, textobjects] fix element.{outer,inner}

--- dotfiles/nvim/after/queries/fennel/textobjects.scm
+++ dotfiles/nvim/after/queries/fennel/textobjects.scm
@@ -5,11 +5,14 @@
 
 (table
   ((_) . (_))* .
-  (_) .
-  (_) @element.inner)
-
-(table
-  ((_) . (_))* .
   (_) @_start .
   (_) @_end
-  (#make-range! "element.outer" @_start @_end))
+  (#make-range! "element.outer" @_start @_end)
+  (#make-range! "element.inner" @_start @_end))
+
+(table_binding
+  ([(_) ":"] . (_))* .
+  [(_) ":"] @_start .
+  (_) @_end
+  (#make-range! "element.outer" @_start @_end)
+  (#make-range! "element.inner" @_start @_end))
