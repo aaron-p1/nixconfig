786e77cf71d0f56275f7f60980e812833afd9839
Aaron Pietscher <aaron.pietscher@gmail.com>
2023-02-12 09:20:41 +0100

    [home-manager, alacritty] add support for ctrl+shift mappings

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -61,10 +61,49 @@ in with lib; {
             }
           ];
         };
-        key_bindings = [{
-          key = "F11";
-          action = "ToggleFullscreen";
-        }];
+        key_bindings = let
+          mod = "Control|Shift";
+          keys = {
+            A = 65;
+            B = 66;
+            # C = 67; # copy
+            D = 68;
+            E = 69;
+            F = 70;
+            G = 71;
+            # H = 72; # copy sha256
+            I = 73;
+            J = 74;
+            K = 75;
+            L = 76;
+            M = 77;
+            N = 78;
+            O = 79;
+            # P = 80; # copy path
+            Q = 81;
+            R = 82;
+            S = 83;
+            T = 84;
+            # U = 85; # open url
+            # V = 86; # paste
+            W = 87;
+            X = 88;
+            Y = 89;
+            Z = 90;
+          };
+
+          ctrlShiftMappings = mapAttrsToList (key: code: {
+            inherit key;
+            mods = mod;
+            chars = "\\x1b[${toString code};6u";
+          }) keys;
+        in concatLists [
+          [{
+            key = "F11";
+            action = "ToggleFullscreen";
+          }]
+          ctrlShiftMappings
+        ];
       };
     };
   };
