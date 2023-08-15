0c031b440bf08df3b980704df3380be6de464805
Aaron Pietscher <aaron.pietscher@gmail.com>
2023-08-15 17:27:21 +0200

    [dotfiles, nvim, queries, php, injections] add more sql strings

--- dotfiles/nvim/after/queries/php/injections.scm
+++ dotfiles/nvim/after/queries/php/injections.scm
@@ -3,6 +3,18 @@
 ((string_value) @sql
   (#match? @sql "^(SELECT|INSERT|UPDATE|DROP)"))
 
+(scoped_call_expression
+  scope: (_) @_scope
+  name: (_) @_name
+  arguments: (arguments . (argument (string (string_value) @sql)))
+  (#any-of? @_scope "\\DB" "DB")
+  (#eq? @_name "raw"))
+
+(scoped_call_expression
+  name: (_) @_name
+  arguments: (arguments . (argument (string (string_value) @sql)))
+  (#any-of? @_name "selectRaw" "whereRaw"))
+
 ((string_value) @regex
   (#lua-match? @regex "^/?%^"))
 
