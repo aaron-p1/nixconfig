5ec88a9a2c29a17b7cc7c33a9113323836c64d3b
Aaron Pietscher <aaron.pietscher@mailbox.org>
2024-01-15 12:09:21 +0100

    [home-manager, hosts, aaron-pc] order ssh keys

--- hosts/aaron-pc/home.nix
+++ hosts/aaron-pc/home.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
   within = {
     git.signingKey = "F524AC445FF173DB!";
+    ssh.keyFiles =
+      [ "~/.ssh/id_ed25519_sk" "~/.ssh/id_ed25519_sk_2" "~/.ssh/id_ed25519" ];
 
     hyprland.monitorFallback = ",highrr,auto,1";
   };
