e45c88215891d4c5d9fd9d353eaa7d2fa4cbd3be
Aaron Pietscher <aaron.pietscher@mailbox.org>
2024-02-18 09:46:32 +0100

    [home-manager, zsh] install zoxide

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -4,58 +4,64 @@ in with lib; {
   options.within.zsh = { enable = mkEnableOption "ZSH"; };
 
   config = mkIf cfg.enable {
-    programs.zsh = {
-      enable = true;
-      enableAutosuggestions = true;
-      enableCompletion = true;
-      syntaxHighlighting = {
+    programs = {
+      zsh = {
         enable = true;
-        styles = {
-          assign = "bold";
-          comment = "fg=#928374";
-          redirection = "fg=magenta";
+        enableAutosuggestions = true;
+        enableCompletion = true;
+        syntaxHighlighting = {
+          enable = true;
+          styles = {
+            assign = "bold";
+            comment = "fg=#928374";
+            redirection = "fg=magenta";
+          };
+        };
+        dotDir = ".config/zsh";
+        shellAliases = {
+          free = "free -h";
+          df = "df -h";
+          cdtmp = "cd $(mktemp -d)";
+
+          o = "xdg-open";
+
+          nvimgit = "nvim +Git +'bdelete 1'";
+          update-nvim-packer = "nvim +PackerSync";
+
+          ns = "NIXPKGS_ALLOW_UNFREE=1 nix shell --impure";
+          nr = "NIXPKGS_ALLOW_UNFREE=1 nix run --impure";
+
+          ssh = "TERM=xterm-256color ssh";
+        };
+        sessionVariables = {
+          LANG = "en_US.UTF-8";
+          LANGUAGE = "en_US.UTF-8";
+          FZF_BASE = "${pkgs.fzf}/share/fzf";
+
+          # vi mode plugin
+          VI_MODE_SET_CURSOR = true;
+          WD_CONFIG = "${config.xdg.configHome}/warprc";
+        };
+        initExtra = ''
+          setopt HIST_IGNORE_ALL_DUPS
+        '';
+        history = {
+          ignoreDups = true;
+          share = false;
+          ignoreSpace = true;
+          path = ".local/share/zsh/zsh_history";
+          save = 100000;
+        };
+        oh-my-zsh = {
+          enable = true;
+          theme = "robbyrussell";
+          plugins = [ "git" "colored-man-pages" "vi-mode" "safe-paste" "wd" ];
         };
       };
-      dotDir = ".config/zsh";
-      shellAliases = {
-        free = "free -h";
-        df = "df -h";
-        cdtmp = "cd $(mktemp -d)";
 
-        o = "xdg-open";
-
-        nvimgit = "nvim +Git +'bdelete 1'";
-        update-nvim-packer = "nvim +PackerSync";
-
-        ns = "NIXPKGS_ALLOW_UNFREE=1 nix shell --impure";
-        nr = "NIXPKGS_ALLOW_UNFREE=1 nix run --impure";
-
-        ssh = "TERM=xterm-256color ssh";
-      };
-      sessionVariables = {
-        LANG = "en_US.UTF-8";
-        LANGUAGE = "en_US.UTF-8";
-        FZF_BASE = "${pkgs.fzf}/share/fzf";
-
-        # vi mode plugin
-        VI_MODE_SET_CURSOR = true;
-        WD_CONFIG = "${config.xdg.configHome}/warprc";
-      };
-      initExtra = ''
-        setopt HIST_IGNORE_ALL_DUPS
-      '';
-      history = {
-        ignoreDups = true;
-        share = false;
-        ignoreSpace = true;
-        path = ".local/share/zsh/zsh_history";
-        save = 100000;
-      };
-      oh-my-zsh = {
+      zoxide = {
         enable = true;
-        theme = "robbyrussell";
-        plugins =
-          [ "git" "fzf" "colored-man-pages" "vi-mode" "safe-paste" "wd" ];
+        enableZshIntegration = true;
       };
     };
   };
