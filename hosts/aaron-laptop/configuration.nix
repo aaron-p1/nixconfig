e552b9c8c9905d4f857ca8ec05aeaa1889a0f815
Aaron Pietscher <aaron.pietscher@gmail.com>
2024-03-08 17:18:32 +0100

    [hosts, aaron-laptop] disable libvirtd

--- hosts/aaron-laptop/configuration.nix
+++ hosts/aaron-laptop/configuration.nix
@@ -47,11 +47,6 @@
     };
   };
 
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = { package = pkgs.qemu_kvm; };
-  };
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- hosts/aaron-laptop/home.nix
+++ hosts/aaron-laptop/home.nix
@@ -1,9 +1,7 @@
-{ pkgs, ... }: {
+_: {
   within = {
     git.signingKey = "F524AC445FF173DB!";
     ssh.keyFiles =
       [ "~/.ssh/id_ed25519_sk" "~/.ssh/id_ed25519_sk_2" "~/.ssh/id_ed25519" ];
   };
-
-  home.packages = with pkgs; [ virt-manager ];
 }
