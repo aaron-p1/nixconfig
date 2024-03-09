20145ef64187a39f965672ca6894d073617b5a36
Aaron Pietscher <aaron.pietscher@mailbox.org>
2024-03-09 11:53:02 +0100

    [dotfiles, nvim, queries, fennel, textobjects] make compatible

--- dotfiles/nvim/after/queries/fennel/textobjects.scm
+++ dotfiles/nvim/after/queries/fennel/textobjects.scm
@@ -1,18 +1,6 @@
 ;; extends
 
-(sequential_table
+(sequence
   (_) @element.inner @element.outer)
 
-(table
-  ((_) . (_))* .
-  (_) @_start .
-  (_) @_end
-  (#make-range! "element.outer" @_start @_end)
-  (#make-range! "element.inner" @_start @_end))
-
-(table_binding
-  ([(_) ":"] . (_))* .
-  [(_) ":"] @_start .
-  (_) @_end
-  (#make-range! "element.outer" @_start @_end)
-  (#make-range! "element.inner" @_start @_end))
+(table_pair) @element.inner @element.outer
