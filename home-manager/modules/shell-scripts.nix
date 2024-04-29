732645b0ed70297a5fd8d3275a615ff2d3be623e
Aaron Pietscher <aaron.pietscher@gmail.com>
2024-04-29 11:14:24 +0200

    [home-manager, shell-scripts] add edit file script

--- home-manager/modules/shell-scripts.nix
+++ home-manager/modules/shell-scripts.nix
@@ -4,6 +4,17 @@ let
 
   cfg = config.within.shellScripts;
 
+  run-editor = pkgs.writeShellScriptBin "e" ''
+    set -eu
+
+    if [ -z "$EDITOR" ]; then
+      echo "EDITOR is not set" >&2
+      exit 1
+    fi
+
+    eval exec $EDITOR "$@"
+  '';
+
   copilot-cli = pkgs.writeShellApplication {
     name = "copilot-cli";
 
@@ -25,5 +36,5 @@ in {
     enable = mkEnableOption "Enable common shell scripts";
   };
 
-  config = mkIf cfg.enable { home.packages = [ copilot-cli ]; };
+  config = mkIf cfg.enable { home.packages = [ run-editor copilot-cli ]; };
 }
