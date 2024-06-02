d9ae2429fb30e698b8ec8a8bd923021f6887b2d9
Aaron Pietscher <aaron.pietscher@mailbox.org>
2024-06-02 14:17:48 +0200

    [home-manager, firefox] add userChrome.js

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -1,14 +1,66 @@
 { config, lib, pkgs, ... }:
 let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) removePrefix removeSuffix mkEnableOption mkIf;
 
   cfg = config.within.firefox;
+
+  toPlainId = id: removePrefix "{" (removeSuffix "}" id);
+
+  sidebery = pkgs.nur.repos.rycee.firefox-addons.sidebery;
+  sideberyId = toPlainId sidebery.addonId;
+
+  # https://www.userchrome.org/what-is-userchrome-js.html#combinedloader
+  firefox = (pkgs.firefox.overrideAttrs (old: {
+    buildCommand = old.buildCommand + # bash
+      ''
+        echo 'pref("general.config.sandbox_enabled", false);' >> $out/lib/firefox/defaults/pref/autoconfig.js
+      '';
+  })).override {
+    extraPrefs = # javascript
+      ''
+        function applyCustomScriptToNewWindow(win){
+          function getElem(elem){
+            return win.document.getElementById(elem);
+          }
+
+          // ctrl+o will be used for Sidebery prev active tab
+          getElem("openFileKb")?.remove();
+          // ctrl+i will be used for Sidebery next active tab
+          getElem("key_viewInfo")?.remove();
+
+          // ctrl+p will be used for Sidebery prev tab
+          getElem("printKb")?.remove();
+          // ctrl+n will be used for Sidebery next tab
+          getElem("key_undoCloseWindow")?.setAttribute("modifiers", "accel,shift,alt");
+          getElem("key_newNavigator")?.setAttribute("modifiers", "accel,shift");
+        }
+
+        /* Single function userChrome.js loader to run the above init function (no external scripts)
+          derived from https://www.reddit.com/r/firefox/comments/kilmm2/ */
+        try {
+          let { classes: Cc, interfaces: Ci, manager: Cm  } = Components;
+          const Services = globalThis.Services;
+          function ConfigJS() { Services.obs.addObserver(this, 'chrome-document-global-created', false); }
+          ConfigJS.prototype = {
+            observe: function (aSubject) { aSubject.addEventListener('DOMContentLoaded', this, {once: true}); },
+            handleEvent: function (aEvent) {
+              let document = aEvent.originalTarget; let window = document.defaultView; let location = window.location;
+              if (/^(chrome:(?!\/\/(global\/content\/commonDialog|browser\/content\/webext-panels)\.x?html)|about:(?!blank))/i.test(location.href)) {
+                if (window._gBrowser) applyCustomScriptToNewWindow(window);
+              }
+            }
+          };
+          if (!Services.appinfo.inSafeMode) { new ConfigJS(); }
+        } catch(ex) {};
+      '';
+  };
 in {
   options.within.firefox = { enable = mkEnableOption "Firefox"; };
 
   config = mkIf cfg.enable {
     programs.firefox = {
       enable = true;
+      package = firefox;
 
       profiles.main = {
         id = 0;
