b5718a46baf806439c8aa20558d4c2b20665507f
Aaron Pietscher <aaron.pietscher@gmail.com>
2024-07-30 10:28:42 +0200

    change git signingKey

--- hosts/aaron-pc/home.nix
+++ hosts/aaron-pc/home.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   within = {
-    git.signingKey = "F524AC445FF173DB!";
+    git.signingKey = "59B8E3B68C81571F!";
     ssh.keyFiles =
       [ "~/.ssh/id_ed25519_sk" "~/.ssh/id_ed25519_sk_2" "~/.ssh/id_ed25519" ];
   };
