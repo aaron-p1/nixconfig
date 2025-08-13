b6eac0a499c6b3f0d22c8c38a514f699d0cd577c
Aaron Pietscher <aaron.pietscher@gmail.com>
2025-08-13 18:28:32 +0200

    [home-manager, shell-scripts] add isolate scripts

--- home-manager/modules/shell-scripts.nix
+++ home-manager/modules/shell-scripts.nix
@@ -17,7 +17,38 @@ let
       exit 1
     fi
 
-    eval exec $EDITOR "$@"
+    exec $EDITOR "$@"
+  '';
+
+  isolate = pkgs.writeShellApplication {
+    name = "isolate";
+    runtimeInputs = [ pkgs.bubblewrap ];
+    text = ''
+      exec bwrap \
+        --unshare-all \
+        --share-net \
+        --die-with-parent \
+        --proc /proc \
+        --dev /dev \
+        --tmpfs /tmp \
+        --ro-bind /nix/store /nix/store \
+        --ro-bind /run/current-system/sw /run/current-system/sw \
+        --bind /run/dbus /run/dbus \
+        --bind /run/user/$UID /run/user/$UID \
+        --symlink "$(readlink /nix/store/mzqrpwsf8q2pv4k6xpgmc84vhnlzvkf8-graphics-drivers)" /run/opengl-driver \
+        --ro-bind /etc/profiles/per-user/aaron /etc/profiles/per-user/aaron \
+        --ro-bind /bin /bin \
+        --ro-bind /usr /usr \
+        --ro-bind /lib /lib \
+        --ro-bind /lib64 /lib64 \
+        --ro-bind /etc /etc \
+        --dir "/home/$USER" \
+        "$@"
+    '';
+  };
+
+  isolate-pwd = pkgs.writeShellScriptBin "isolate-pwd" ''
+    exec ${isolate}/bin/isolate --bind "$PWD" "$PWD" "$@"
   '';
 in
 {
@@ -25,5 +56,11 @@ in
     enable = mkEnableOption "Enable common shell scripts";
   };
 
-  config = mkIf cfg.enable { home.packages = [ run-editor ]; };
+  config = mkIf cfg.enable {
+    home.packages = [
+      run-editor
+      isolate
+      isolate-pwd
+    ];
+  };
 }
