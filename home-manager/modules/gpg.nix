d7c4d31bdd95fd9f930ed2c0c465475eae05bf39
Aaron Pietscher <aaron.pietscher@mailbox.org>
2026-01-26 17:15:07 +0100

    [home-manager, gpg] stop overriding pinentry

--- home-manager/modules/gpg.nix
+++ home-manager/modules/gpg.nix
@@ -17,10 +17,6 @@ in
   config = mkIf cfg.enable {
     programs.gpg = {
       enable = true;
-      package = pkgs.gnupg.override {
-        guiSupport = true;
-        pinentry = pkgs.pinentry-qt;
-      };
       homedir = "${config.xdg.dataHome}/gnupg";
     };
 
