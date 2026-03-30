a0b21bcd5cb35e86ef80d00b5f35e8362a5347e0
Aaron Pietscher <aaron.pietscher@mailbox.org>
2026-03-30 14:05:22 +0200

    [nixos, nix] add binary cache for cuda

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -46,9 +46,13 @@ in
       settings = {
         trusted-users = [ "root" ];
 
-        substituters = [ "https://nix-community.cachix.org/" ];
+        substituters = [
+          "https://nix-community.cachix.org/"
+          "https://cache.nixos-cuda.org"
+        ];
         trusted-public-keys = [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
         ];
 
         auto-optimise-store = true;
