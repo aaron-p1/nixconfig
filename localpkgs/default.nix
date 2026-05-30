9e67dee8ac8492c48668e359c0220d1ff8c394b2
Aaron Pietscher <aaron.pietscher@mailbox.org>
2026-05-30 11:16:41 +0200

    [localpkgs] remove nvidia-patched and nvlax

--- localpkgs/default.nix
+++ localpkgs/default.nix
@@ -1,15 +1,9 @@
 { inputs, ... }:
 final: prev: {
-  local = rec {
+  local = {
     initdev = prev.callPackage ./pkgs/initdev { };
 
     # firefox native messaging hosts
     ff2mpv-native-client = prev.callPackage ./pkgs/ff2mpv-native-client { };
-
-    # Nvidia
-    nvlax = prev.callPackage ./pkgs/nvlax { };
-    # patched with https://github.com/keylase/nvidia-patch
-    nvidia-patched =
-      nvidia_x11: prev.callPackage ./overrides/nvidia-patched { inherit nvidia_x11 nvlax; };
   };
 }
