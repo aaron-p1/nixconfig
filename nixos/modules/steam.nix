89bca66c52a425830b8b20e1830a91a34971545c
Aaron Pietscher <aaron.pietscher@mailbox.org>
2026-05-30 12:13:35 +0200

    [nixos, steam] remove aftersteam script

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -25,25 +25,5 @@ in
 
       extraCompatPackages = [ pkgs.proton-ge-bin ];
     };
-
-    environment.systemPackages = [
-      (pkgs.writeTextFile {
-        name = "aftersteam";
-        destination = "/bin/aftersteam";
-        executable = true;
-        text = ''
-          #!${pkgs.bash}/bin/bash
-
-          while ${pkgs.procps}/bin/pgrep steamwebhelper > /dev/null
-          do
-            echo "$(${pkgs.coreutils}/bin/date "+%Y-%m-%d %H:%M:%S"): Still Running, waiting 10s"
-            sleep 10
-          done
-
-          echo "Stopped, waiting 3s"
-          sleep 3
-        '';
-      })
-    ];
   };
 }
